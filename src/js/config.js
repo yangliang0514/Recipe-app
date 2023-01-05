b040a6ca5ff1820b8ac9bad4d3f337f4fc2afddc
yangliang0514 <e510021@gmail.com>
2023-01-05 23:01:57 +0800

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = "https://forkify-api.herokuapp.com/api/v2/recipes/";
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = "2139147d-e990-4463-9795-be50b7dae34f";
+export const MODAL_CLOSE_SEC = 2.5;
